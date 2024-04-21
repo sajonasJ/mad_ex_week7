5efb372a2d28e428262c37f030e1a7aacc4306fd
Larry Wen <l.wen@griffith.edu.au>
2024-04-21 14:33:34 +1000

    - initial project

---
+++ ex.test.js
@@ -0,0 +1,15 @@
+/**
+ * @file ex.test.js
+ * @description Test suite for Week 7 exercises of the Mobile Application Development
+ *              course. This file contains test cases that validate the correctness of
+ *              the code written in the exercises. The tests are designed to assess
+ *              the functionality of the implemented solutions against specified
+ *              requirements and scenarios.
+ * @author Larry Wen
+ * @created [21/04/2024]
+ *
+ * NOTE: This file is not intended to be modified by the students. Alterations to this
+ * test suite may result in inaccurate assessments of the exercise solutions.
+ */
+
+const {} = require("./questions");
